af725a3065693553c82c7f2f4b16cf81f7033f92
Lamshuwa <140602212+Lamshuwa@users.noreply.github.com>
2023-07-29 20:36:38 +0530

    Add New Files

---
+++ First_program/src/First_Class.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class First_Class {
+
+public static void main(String[] args) {
+
+		//printing
+//		String a = "HelloWorld!";
+//		System.out.println(a);
+		
+		//scanning
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter Any Year?");
+//		int age = sc.nextInt();		
+//		System.out.println("Today's Year is: "+age);
+		
+		//Conditional
+		int age=24;
+		if(age<18) {
+			System.out.println("NO");
+		}
+			else {
+				System.out.println("Yes");
+			}
+		
+		
+}	
+}
